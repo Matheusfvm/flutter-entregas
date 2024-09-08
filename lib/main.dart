496440baa41b9c7586a73ae7ae226b62aa7d9554
Matheus Melo <matheusfernando.vm@gmail.com>
2024-09-08 12:22:35 -0300

    Terminando a entrega da atividade

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       routes: {
         "/": (context) => const LoginPage(),
-        "/listaProdutos": (context) => const LoginPage(),
+        "/listaProdutos": (context) => const ProductListPage(),
+        "/confirmacao": (context) => const ConfirmationPage(),
       },
     );
   }
@@ -43,7 +44,7 @@ class LoginPageState extends State<LoginPage> {
   void _enviar() {
     String email = _email.text;
     String senha = _senha.text;
-    if (email == _usuarioEmail || senha == _usuarioSenha) {
+    if (email == _usuarioEmail && senha == _usuarioSenha) {
       Navigator.pushReplacementNamed(context, "/listaProdutos");
     }
   }
@@ -57,10 +58,10 @@ class LoginPageState extends State<LoginPage> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            const SizedBox(height: 100.0), // um retângulo para separar widgets
+            const SizedBox(height: 200.0), // um retângulo para separar widgets
             Image.asset('assets/images/logo.png'),
             const SizedBox(
-                height: 300.0), // um retângulo contendo widget de entrada
+                height: 200.0), // um retângulo contendo widget de entrada
             SizedBox(
                 // label para primeiro número
                 width: 300,
@@ -72,9 +73,7 @@ class LoginPageState extends State<LoginPage> {
                       hintText: 'Login',
                       prefixIcon: Icon(Icons.account_circle_outlined),
                       enabledBorder: OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue),
-                      )),
+                      focusedBorder: OutlineInputBorder()),
                 )),
             const SizedBox(height: 16.0),
             const SizedBox(
@@ -86,20 +85,19 @@ class LoginPageState extends State<LoginPage> {
                   controller: _senha, // associa controle ao widget
                   keyboardType: TextInputType.text, // tipo de entrada
                   decoration: const InputDecoration(
-                    // customização
-                    hintText: 'Senha',
-                    prefixIcon: Icon(Icons.account_circle_outlined), //icon
-                    enabledBorder: OutlineInputBorder(),
-                    //quando receber o foco, altera cor da borda
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                  ),
+                      // customização
+                      hintText: 'Senha',
+                      prefixIcon: Icon(Icons.account_circle_outlined), //icon
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder()
+                      //quando receber o foco, altera cor da borda
+                      ),
                 )),
             const SizedBox(height: 16.0),
             // if ternário que controla exibição da resposta.
             // senão foi enviado, então apresenta botões
             // enviar e cancelar
+            const SizedBox(height: 16.0),
             SizedBox(
                 // botões
                 width: 300,
@@ -122,14 +120,14 @@ class LoginPageState extends State<LoginPage> {
   }
 }
 
-class ProductsPage extends StatefulWidget {
-  const ProductsPage({super.key});
+class ProductListPage extends StatefulWidget {
+  const ProductListPage({super.key});
 
   @override
-  ProductsPageState createState() => ProductsPageState();
+  ProductListPageState createState() => ProductListPageState();
 }
 
-class ProductsPageState extends State<ProductsPage> {
+class ProductListPageState extends State<ProductListPage> {
   final List<String> produtos = [
     "Produto 1",
     "Produto 2",
@@ -141,6 +139,158 @@ class ProductsPageState extends State<ProductsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const Divider(),
+            const SizedBox(height: 16.0),
+            const Text(
+              "Lista de Produtos",
+              style: TextStyle(fontSize: 24.0, color: Colors.black),
+            ),
+            const SizedBox(height: 16.0),
+            Container(
+              width: 500,
+              height: 200,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1,
+                ),
+              ),
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: produtos.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(produtos[index]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                ProductPage(product: produtos[index])),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductPage extends StatelessWidget {
+  final String product;
+  const ProductPage({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 100.0),
+            const Text(
+              "Detalhes da compra",
+              style: TextStyle(fontSize: 24.0, color: Colors.black),
+            ),
+            const SizedBox(height: 32.0),
+            SizedBox(
+              width: 300,
+              child: TextFormField(
+                decoration: InputDecoration(
+                    hintText: product,
+                    enabledBorder: const OutlineInputBorder(),
+                    focusedBorder: const OutlineInputBorder()),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
+            SizedBox(
+              width: 300,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    hintText: "Quantidade",
+                    enabledBorder: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder()),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
+            SizedBox(
+                // botões
+                width: 300,
+                // Row determina que os widgets serão acrescentados
+                // lado a lado
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(
+                            context, "/listaProdutos");
+                      }, // executa _enviar
+                      child: const Text('Voltar'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, "/confirmacao");
+                      }, // executa _enviar
+                      child: const Text('Enviar'),
+                    ),
+                  ],
+                )),
+            const SizedBox(height: 16.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ConfirmationPage extends StatelessWidget {
+  const ConfirmationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 200.0), // um retângulo para separar widgets
+            Image.asset('assets/images/logo.png'),
+            const SizedBox(height: 100.0),
+            const Text(
+              "Pedido \nConfirmado!",
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 100.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, "/listaProdutos");
+              }, // executa _enviar
+              child: const Text('Voltar'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
