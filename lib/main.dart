9fb38947d021dc155bda0cb05da4f505d096c976
Mathes Melo <matheusfernando.vm@gmail.com>
2024-09-09 12:05:01 -0300

    Adicionando o código inicial da entrega 2

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:collection';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  List<dynamic> userList = [];
+
+  Future<void> fetchData() async {
+    final response =
+        await http.get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
+
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(response.body);
+
+      setState(() {
+        userList = jsonResponse;
+      });
+    } else {
+      print("Requisição falhou com o status: ${response.statusCode}.");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(),
+          body: ListView.builder(
+            itemCount: userList.length,
+            itemBuilder: (BuildContext context, int index) {
+              final user = userList[index];
+              return Container(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                decoration: BoxDecoration(
+                  color: (index % 2 == 0 ? Colors.blue : Colors.green),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              );
+            },
+          )),
+    );
+  }
+}
